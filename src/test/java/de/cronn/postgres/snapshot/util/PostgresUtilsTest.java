cronn/postgres-snapshot-util

commit 6f96ff4607b5842a5e0e2fb665bb8039dba4d40b

Refactor localhost check in PostgresUtils

src/main/java/de/cronn/postgres/snapshot/util/PostgresUtils.java
@@ -28,7 +28,7 @@ final class PostgresUtils {
 		URI databaseUri = toUri(jdbcUrl);
 
 		int port = databaseUri.getPort();
-		String host = resolveHost(databaseUri.getHost());
+		String host = prepareHostname(databaseUri.getHost());
 
 		Properties connectionProperties = new Properties();
 		connectionProperties.put("user", username);
@@ -52,21 +52,21 @@ final class PostgresUtils {
 		}
 	}
 
-	private static String resolveHost(String host) {
-		try {
-			InetAddress inetAddress = InetAddress.getByName(host);
-			if (isLocalhost(inetAddress)) {
-				return PostgresConstants.DOCKER_HOST_INTERNAL;
-			} else {
-				return inetAddress.getHostAddress();
-			}
-		} catch (UnknownHostException e) {
-			throw new IllegalArgumentException("Failed to resolve host", e);
+	private static String prepareHostname(String host) {
+		if (isLocalhost(host)) {
+			return PostgresConstants.DOCKER_HOST_INTERNAL;
+		} else {
+			return host;
 		}
 	}
 
-	private static boolean isLocalhost(InetAddress inetAddress) throws UnknownHostException {
-		return InetAddress.getByName("localhost").equals(inetAddress);
+	private static boolean isLocalhost(String host) {
+		try {
+			InetAddress localHost = InetAddress.getLocalHost();
+			return localHost.getHostName().equals(host) || localHost.getHostAddress().equals(host);
+		} catch (UnknownHostException e) {
+			throw new RuntimeException("Failed resolve localhost", e);
+		}
 	}
 
 	static String deriveNetworkMode(ConnectionInformation connectionInformation) {

src/test/java/de/cronn/postgres/snapshot/util/PostgresUtilsTest.java
@@ -3,7 +3,6 @@ package de.cronn.postgres.snapshot.util;
 import static org.assertj.core.api.Assertions.*;
 
 import java.net.URISyntaxException;
-import java.net.UnknownHostException;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,10 +18,9 @@ class PostgresUtilsTest {
 
 	@Test
 	void testParseConnectionInformation_unknownHost() {
-		assertThatExceptionOfType(IllegalArgumentException.class)
-			.isThrownBy(() -> PostgresUtils.parseConnectionInformation("jdbc:postgresql://unknown-host", "user", "password"))
-			.withCauseExactlyInstanceOf(UnknownHostException.class)
-			.withMessage("Failed to resolve host");
+		assertThatExceptionOfType(RuntimeException.class)
+			.isThrownBy(() -> PostgresUtils.parseConnectionInformation("jdbc:postgresql://unknown-host/test", "user", "password"))
+			.withMessage("org.postgresql.util.PSQLException: The connection attempt failed.");
 	}
 
 	@Test

commit 2f21f50c4c6324f6d770596bc666593d32fc61c3

Fix PostgresUtils.isLocalhost()
This fixes a regression that was introduced with 6f96ff4607b5842a5e0e2fb665bb8039dba4d40b

src/main/java/de/cronn/postgres/snapshot/util/PostgresUtils.java
@@ -142,12 +142,21 @@ final class PostgresUtils {
 		}
 	}
 
-	private static boolean isLocalhost(String host) {
+	static boolean isLocalhost(String host) {
+		if (host.equals("localhost")) {
+			return true;
+		}
+		InetAddress inetAddress;
 		try {
-			InetAddress localHost = InetAddress.getLocalHost();
-			return localHost.getHostName().equals(host) || localHost.getHostAddress().equals(host);
+			inetAddress = InetAddress.getByName(host);
 		} catch (UnknownHostException e) {
-			throw new RuntimeException("Failed resolve localhost", e);
+			log.trace("Failed to resolve host '{}'", host, e);
+			return false;
+		}
+		try {
+			return InetAddress.getByName("localhost").equals(inetAddress);
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e);
 		}
 	}
 

src/test/java/de/cronn/postgres/snapshot/util/PostgresUtilsTest.java
@@ -2,9 +2,12 @@ package de.cronn.postgres.snapshot.util;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.net.InetAddress;
 import java.net.URISyntaxException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PostgresUtilsTest {
 
@@ -24,6 +27,13 @@ class PostgresUtilsTest {
 	}
 
 	@Test
+	void testParseConnectionInformation_failsToConnect() {
+		assertThatExceptionOfType(RuntimeException.class)
+			.isThrownBy(() -> PostgresUtils.parseConnectionInformation("jdbc:postgresql://10.0.0.1/test?connectTimeout=1", "user", "password"))
+			.withMessage("org.postgresql.util.PSQLException: The connection attempt failed.");
+	}
+
+	@Test
 	void testParseConnectionInformation_driverFailsToParseJdbcUrl() {
 		assertThatExceptionOfType(RuntimeException.class)
 			.isThrownBy(() -> PostgresUtils.parseConnectionInformation("jdbc:postgresql://localhost", "user", "password"))
@@ -36,5 +46,19 @@ class PostgresUtilsTest {
 			.isThrownBy(() -> PostgresUtils.parseConnectionInformation("jdbc:postgresql://localhost/test", "user", "password"))
 			.withMessage("org.postgresql.util.PSQLException: Connection to localhost:5432 refused." +
 						 " Check that the hostname and port are correct and that the postmaster is accepting TCP/IP connections.");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "localhost", "127.0.0.1" })
+	void testIsLocalHost_positive(String host) {
+		assertThat(PostgresUtils.isLocalhost(host)).isTrue();
+	}
+
+	@Test
+	void testIsLocalHost_negative() throws Exception {
+		assertThat(PostgresUtils.isLocalhost("some.host.local")).isFalse();
+		assertThat(PostgresUtils.isLocalhost("1.2.3.4")).isFalse();
+		InetAddress localHost = InetAddress.getLocalHost();
+		assertThat(PostgresUtils.isLocalhost(localHost.getHostName())).isFalse();
 	}
 }