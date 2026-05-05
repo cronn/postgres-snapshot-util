ee3d731cdc30a1c81df3645508116d35bd587d05
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2026-05-05 09:26:49 +0200

    Convert build scripts to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,194 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
+
+plugins {
+    `java-library`
+    `maven-publish`
+    jacoco
+    signing
+    id("org.jreleaser") version "latest.release"
+    id("com.diffplug.spotless") version "latest.release"
+}
+
+group = "de.cronn"
+version = "1.7-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+    maxHeapSize = "256m"
+    inputs.dir("data/test/validation")
+    outputs.dir("data/test/output")
+    outputs.dir("data/test/tmp")
+}
+
+dependencyLocking {
+    lockAllConfigurations()
+}
+
+tasks.wrapper {
+    gradleVersion = "9.5.0"
+    distributionType = Wrapper.DistributionType.ALL
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+    dependsOn(tasks.test)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier = "sources"
+    from(sourceSets.main.get().allSource)
+    dependsOn(tasks.classes)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier = "javadoc"
+    from(tasks.javadoc.get().destinationDir)
+    dependsOn(tasks.javadoc)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            pom {
+                name = project.name
+                description = "Snapshot Utilities for PostgreSQL"
+                url = "https://github.com/cronn/postgres-snapshot-util"
+
+                licenses {
+                    license {
+                        name = "The Apache Software License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                        distribution = "repo"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "benedikt.waldvogel"
+                        name = "Benedikt Waldvogel"
+                        email = "benedikt.waldvogel@cronn.de"
+                    }
+                    developer {
+                        id = "ayk.borstelmann"
+                        name = "Ayk Borstelmann"
+                        email = "ayk.borstelmann@cronn.de"
+                    }
+                }
+
+                scm {
+                    url = "https://github.com/cronn/postgres-snapshot-util"
+                }
+            }
+
+            from(components["java"])
+
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "staging"
+            url = uri(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
+
+jreleaser {
+    signing {
+        setActive("NEVER")
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    setActive("RELEASE")
+                    sign = false
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                }
+            }
+        }
+    }
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    kotlinGradle {
+        ktlint()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:latest.release")
+    implementation("org.testcontainers:testcontainers:latest.release")
+    implementation("org.apache.commons:commons-lang3:latest.release")
+
+    runtimeOnly("org.postgresql:postgresql:latest.release")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.assertj:assertj-core:latest.release")
+    testImplementation("de.cronn:test-utils:latest.release")
+    testImplementation("de.cronn:validation-file-assertions:latest.release")
+    testImplementation("org.testcontainers:testcontainers-postgresql:latest.release")
+
+    testRuntimeOnly("ch.qos.logback:logback-classic:latest.release")
+
+    components {
+        all {
+            if (id.version.matches(Regex("(?i).+([-.])(CANDIDATE|RC|BETA|ALPHA|M\\d+).*"))) {
+                status = "milestone"
+            }
+        }
+    }
+}
